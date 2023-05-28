af0469101d159554a35d25adae48f4810b0c86de
Nabegon <watami03@gmail.com>
2023-05-28 12:59:31 +0200

    feat(share images): add image share button function with hard-corded code

--- zoo_app/lib/main.dart
+++ zoo_app/lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,24 +18,39 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: '7 Sterne'),
+      home: const MyAppGridView(title: '7 Sterne'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
+class MyAppGridView extends StatefulWidget {
+  const MyAppGridView({Key? key, required this.title}) : super(key: key);
   final String title;
+
+  @override
+  _MyAppGridViewState createState() => _MyAppGridViewState();
+}
+
+class _MyAppGridViewState extends State<MyAppGridView> {
+  Future<void> shareImage() async {
+    try {
+      final directory = await getApplicationDocumentsDirectory();
+      final imageFile = await File('${directory.path}/my_image.png').create();
+      await imageFile.writeAsBytes((await rootBundle.load('assets/images/my_image.png')).buffer.asUint8List());
+      await Share.shareFiles([imageFile.path], mimeTypes: ['image/png']);
+    } catch (e) {
+      print('error: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: DefaultTextStyle( // Used DefaultTextStyle
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 30), // Made title bold
-            child: Text(title),
-          ),
+        title: DefaultTextStyle(
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 30), // Made title bold
+          child: Text(widget.title),
+        ),
         centerTitle: true,
         backgroundColor: Color(0xfff47b20),
       ),
@@ -50,32 +69,43 @@ class MyHomePage extends StatelessWidget {
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(20),
               border: Border.all(
-                  color: Color(0xff640000),
-                  width: 3,
+                color: Color(0xff640000),
+                width: 3,
               ),
+              image: index == 0 ? DecorationImage(
+                image: AssetImage('assets/images/my_image.png'),
+                fit: BoxFit.cover,
+              ) : null,
             ),
             child: Stack(
               children: <Widget>[
-                Center(child: Text("Name?", style: TextStyle(color: Color(0xff640000)))),
-                Positioned( // added Positioned
-                  top: 5, // from top 5
-                  right: 5, // from right 5
-                  child: Icon(Icons.star, color: Colors.grey, size: 40,),
+                Center(child: index == 0 ? Text(" ") : Text("Name?", style: TextStyle(color: Color(0xff640000)))),
+                Positioned(
+                  top: 5,
+                  right: 5,
+                  child: Icon(Icons.star, color: index == 0 ? Colors.yellow : Colors.grey, size: 40,),
                 ),
-                Positioned( // added Positioned
-                  bottom: 5, // from bottom 5
-                  left: 5, // from left 5
-                  child: Icon(Icons.add_a_photo, color: Color(0xfff47b20), size: 40,), // added add_a_photo Icon
+                Positioned(
+                  bottom: 5,
+                  left: 5,
+                  child: Icon(Icons.add_a_photo, color: Color(0xfff47b20), size: 40,),
                 ),
-                Positioned( // added Positioned
-                  bottom: 3, // from bottom 5
-                  left: 45, // from left 5
-                  child: Icon(Icons.add_comment, color: Color(0xfff47b20), size: 40,), // added add_a_photo Icon
+                Positioned(
+                  bottom: 3,
+                  left: 45,
+                  child: InkWell(
+                    onTap: () {
+                      if (index == 0) {
+                        shareImage();
+                      }
+                    },
+                    child: Icon(Icons.add_comment, color: Color(0xfff47b20), size: 40,),
+                  ),
                 ),
-                Positioned( // added Positioned
-                  bottom: 12, // from bottom 5
-                  left: 80, // from left 5
-                  child: Icon(Icons.question_mark, color: Color(0xfff47b20), size: 30,), // added add_a_photo Icon
+                Positioned(
+                  bottom: 12,
+                  left: 80,
+                  child: Icon(Icons.question_mark, color: Color(0xfff47b20), size: 30,),
                 ),
               ],
             ),
